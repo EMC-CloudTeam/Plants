0b2ae13718c7b34ae9f584e1ece6f2c972a702a2
Senthil <senthil@dev.pushpointmobile.com>
2012-06-04 10:20:59 +0530

    maven modules

--- plants-web/src/main/main/java/com/emc/plants/web/App.java
+++ plants-web/src/main/main/java/com/emc/plants/web/App.java
@@ -1,10 +1,10 @@
-package com.emc.plants;
+package com.emc.plants.service;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
