e79d7a01af74b925e350b9b03f89fdc370e41ad6
Senthil <senthil@dev.pushpointmobile.com>
2012-06-12 11:40:40 +0530

    account servlet changes

--- plants-web/src/main/java/com/emc/plants/web/servlets/AccountServlet.java
+++ plants-web/src/main/java/com/emc/plants/web/servlets/AccountServlet.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-i
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
